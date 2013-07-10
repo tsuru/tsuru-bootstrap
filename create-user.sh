ffea9c68e29158683a8266f1e631ff27a42ca6fb
Damian Gryski <damian@gryski.com>
2013-07-10 21:12:42 +0200

    genericize -- make applicable to other people

--- create-user.sh
+++ create-user.sh
@@ -4,7 +4,7 @@ set -x
 tsuru target-add default tsuru.lan:8080
 tsuru target-set default
 
-tsuru user-create dgryski@example.com
-tsuru login dgryski@example.com
-tsuru team-create dgryski
-tsuru key-add ~/.ssh/github_rsa.pub 
+tsuru user-create $USER@example.com
+tsuru login $USER@example.com
+tsuru team-create $USER
+tsuru key-add ~/.ssh/id_rsa.pub
