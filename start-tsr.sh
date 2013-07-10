b18190cee1bc3cfd4c2b3b3a0fa4e549c6807e64
Damian Gryski <damian@gryski.com>
2013-07-10 23:38:17 +0200

    backgrounding sudo when it asks for a password isn't nice

--- start-tsr.sh
+++ start-tsr.sh
@@ -6,6 +6,9 @@ sudo pkill -u ubuntu tsr
 bindir=/usr/bin
 [ -x ~vagrant/.go/bin/tsr ] && bindir=~vagrant/.go/bin/
 
+# cache credentials -- prompting and '&' don't play nicely together
+sudo -u ubuntu /bin/true
+
 echo "starting tsuru-collector"
 sudo -u ubuntu $bindir/tsr collector &
 echo "starting tsuru-api"
