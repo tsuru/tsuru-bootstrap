87fdfc8e24ecf09cde5c45aee6805dd7a73dc578
Damian Gryski <damian@gryski.com>
2013-07-11 09:49:31 +0200

    fix sudo config to make automated installs easier

--- install-tsuru.sh
+++ install-tsuru.sh
@@ -3,6 +3,9 @@
 # faster for me
 sudo perl -pi -e 's/us.archive.ubuntu.com/nl.archive.ubuntu.com/'  /etc/apt/sources.list
 
+# fix "sudo -u" prompting for password
+echo "%vagrant ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/vagrant
+
 sudo apt-get -y install curl
 
 curl -O https://raw.github.com/globocom/tsuru/master/misc/docker-setup.bash
