0361679abf35265228321ac552032a43efa8e98b
Damian Gryski <damian@gryski.com>
2013-07-13 14:14:33 +0200

    move hipache to background -- stop spamming my console with status messages.

--- install-tsuru.sh
+++ install-tsuru.sh
@@ -11,6 +11,10 @@ sudo apt-get -y install curl
 curl -O https://raw.github.com/globocom/tsuru/master/misc/docker-setup.bash
 /bin/bash docker-setup.bash
 
+# move hipache to background
+sudo pkill node
+sudo start-stop-daemon --background --start --make-pidfile --pidfile /var/run/hipache.pid --exec /usr/local/bin/hipache -- --config /etc/hipache.conf.json
+
 # create docker images
 git clone https://github.com/flaviamissi/basebuilder
 (cd basebuilder/python/ && docker build -t "tsuru/python" .)
