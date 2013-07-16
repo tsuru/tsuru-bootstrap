2cb4eba75dcf7c3f9236659df332de0354cc6bf1
Damian Gryski <damian@gryski.com>
2013-07-16 22:54:27 +0200

    fix docker args

--- heroku/install.sh
+++ heroku/install.sh
@@ -2,7 +2,7 @@
 
 #curl -O https://s3.amazonaws.com/progrium-dokku/progrium_buildstep.tgz
 cat /vagrant/progrium_buildstep.tgz | gunzip -cd | docker import - progrium/buildstep
-docker -t build tsuru/heroku - < Dockerfile
+docker build -t tsuru/heroku - < Dockerfile
 
 # TODO: export PORT=8888
 
