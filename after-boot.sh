2a1244a2bebe5adb45d7d05fccebb5e003980ac6
Damian Gryski <damian@gryski.com>
2013-07-18 01:05:01 +0200

    start git daemon

--- after-boot.sh
+++ after-boot.sh
@@ -5,5 +5,6 @@ set -x
 # restart the required services after bootup
 sudo start-stop-daemon --background --start --exec ~/.go/bin/docker -- -d
 sudo start-stop-daemon --background --start --make-pidfile --pidfile /var/run/hipache.pid --exec /usr/local/bin/hipache -- --config /etc/hipache.conf.json
+sudo -u git -i start-stop-daemon --start --background --exec /usr/bin/git -- daemon --base-path=/var/repositories --syslog --export-all
 sudo -u git start-stop-daemon --background --start --exec /usr/bin/gandalf-webserver
 sh /vagrant/start-tsr.sh
