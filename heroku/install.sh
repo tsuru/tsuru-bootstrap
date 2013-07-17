0671ac54c3fa9553bad12478c277ac19dbf8d116
Damian Gryski <damian@gryski.com>
2013-07-18 01:11:51 +0200

    add heroku platform

--- heroku/install.sh
+++ heroku/install.sh
@@ -4,5 +4,9 @@
 cat /vagrant/progrium_buildstep.tgz | gunzip -cd | docker import - progrium/buildstep
 docker build -t tsuru/heroku - < Dockerfile
 
+mongo tsuru <<EOJS
+db.platforms.save({"_id":"heroku"})
+EOJS
+
 # TODO: export PORT=8888
 
