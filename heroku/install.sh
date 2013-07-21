f0ee95bbad0a4dd4d5b0619555df84cf44b876d3
Damian Gryski <damian@gryski.com>
2013-07-21 22:05:53 +0200

    fix docker build line now that we need 'add'

--- heroku/install.sh
+++ heroku/install.sh
@@ -4,7 +4,7 @@ set -x
 
 #curl -O https://s3.amazonaws.com/progrium-dokku/progrium_buildstep.tgz
 cat /vagrant/progrium_buildstep.tgz | gunzip -cd | docker import - progrium/buildstep
-docker build -t tsuru/heroku - < Dockerfile
+docker build -t tsuru/heroku .
 
 mongo tsuru <<EOJS
 db.platforms.save({"_id":"heroku"})
