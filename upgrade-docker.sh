779562075ef9b0898e5ea3b58a8a6c6e8c3248fd
Andrews Medina <andrewsmedina@gmail.com>
2014-05-27 18:50:05 -0300

    remove unused files.

--- upgrade-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Upgrade go
-. ~/.go_bashrc
-go get -v github.com/dotcloud/docker/docker
-go install github.com/dotcloud/docker/docker
-
-sudo pkill docker
-sudo start-stop-daemon --background --start --exec ~/.go/bin/docker -- -d -H 0.0.0.0:4243
